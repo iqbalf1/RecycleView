8df36c1a37fb905336491aa31db79db48c191b06
iqbalfadillah <iqbalfadillah408@gmail.com>
2022-04-04 02:03:47 +0700

    with binding

--- app/src/main/java/com/binar/recyclerviewsample/ListFragment.kt
+++ app/src/main/java/com/binar/recyclerviewsample/ListFragment.kt
@@ -9,22 +9,25 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.binar.recyclerviewsample.binar.UserAdapter
+import com.binar.recyclerviewsample.databinding.ActivityMainBinding
+import com.binar.recyclerviewsample.databinding.FragmentListBinding
+import java.util.zip.Inflater
 
 
 class ListFragment : Fragment() {
-
+    private lateinit var binding  : FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycleview)
         val userList = listOf(
             Contacts("Iqbal Fadillah",85601005817),
             Contacts("Ahmad Sopian",8574371936),
@@ -36,8 +39,8 @@ class ListFragment : Fragment() {
             findNavController().navigate(action)
         })
         val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false)
-        recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = userAdapter
+        binding.recycleview.layoutManager = layoutManager
+        binding.recycleview.adapter = userAdapter
     }
 
 
--- app/src/main/java/com/binar/recyclerviewsample/binar/UserAdapter.kt
+++ app/src/main/java/com/binar/recyclerviewsample/binar/UserAdapter.kt
@@ -9,27 +9,28 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.binar.recyclerviewsample.Contacts
 import com.binar.recyclerviewsample.R
+import com.binar.recyclerviewsample.databinding.ItemUserBinding
 
 class UserAdapter(private val contacts : List<Contacts>,
                   private var onItemClick : (Contacts)->Unit) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
-    class UserViewHolder(view: View):RecyclerView.ViewHolder(view){
+    class UserViewHolder(val binding: ItemUserBinding):RecyclerView.ViewHolder(binding.root){
 
-        val usernameTextView = view.findViewById<TextView>(R.id.username_textView)
-        val phoneNumberTextView = view.findViewById<TextView>(R.id.phonenumber_textView)
-        val root:LinearLayout = view.findViewById(R.id.root)
+        val usernameTextView = binding.usernameTextView
+        val phoneNumberTextView = binding.phonenumberTextView
+        val roots:LinearLayout = binding.roots
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user,parent,false)
-        return UserViewHolder(view)
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         holder.usernameTextView.text = contacts[position].username
         holder.phoneNumberTextView.text = contacts[position].phoneNumber.toString()
-        holder.root.setOnClickListener {
+        holder.roots.setOnClickListener {
             onItemClick(contacts[position])
         }
 
